a9ab3f1b340a18573ba36aa20d6d607ac50345ed
etcher3rd <etcher3rd@gmail.com>
2015-02-04 15:47:13 +0100

    ajout de l'export pour FC3

--- katze.lua
+++ katze.lua
@@ -1,17 +1,22 @@
 function rendre_hommage_au_grand_Katze()
-	local myInfo = LoGetSelfData()
-	if myInfo.Name ~= gCurrentAircraft then
-		gCurrentAircraft = myInfo.Name
-		if gCurrentAircraft == "A-10C" then
-		elseif gCurrentAircraft == "Ka-50" then
-			dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_KA50.lua');
-		elseif gCurrentAircraft == "Mi-8MT" then
-			dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_Mi8.lua');
-		elseif gCurrentAircraft == "UH-1H" then
-			dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_UH1.lua');
-		elseif gCurrentAircraft == "P-51D" then
-		else
-			dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_FC3.lua');
-		end   
-	end  
+	local lDevice = GetDevice(0)	
+	if type(lDevice) == "table" then
+		local myInfo = LoGetSelfData()
+		if myInfo.Name ~= gCurrentAircraft then
+			gCurrentAircraft = myInfo.Name
+			if gCurrentAircraft == "A-10C" then
+			elseif gCurrentAircraft == "Ka-50" then
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_KA50.lua');
+			elseif gCurrentAircraft == "Mi-8MT" then
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_Mi8.lua');
+			elseif gCurrentAircraft == "UH-1H" then
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_UH1.lua');
+			elseif gCurrentAircraft == "P-51D" then
+			else
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_FC3.lua');
+			end   
+		end  
+	else
+		dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_FC3.lua');
+	end
 end
