dd2b683c94e0492ac83959fb4360dbc4bd3cd6d3
etcher3rd <etcher3rd@gmail.com>
2015-02-10 21:18:47 +0100

    derp

--- katze.lua
+++ katze.lua
@@ -1,36 +1,24 @@
 exportFC3done = false
 function rendre_hommage_au_grand_Katze()
-	la_petite_chatte = false
-	local lDevice = GetDevice(0)
+	local lDevice = GetDevice(0)	
 	if type(lDevice) == "table" then
 		local myInfo = LoGetSelfData()
 		if myInfo.Name ~= gCurrentAircraft then
 			gCurrentAircraft = myInfo.Name
 			if gCurrentAircraft == "A-10C" then
 			elseif gCurrentAircraft == "Ka-50" then
-				la_petite_chatte = '/Scripts/KTZ_SIOC_KA50.lua'
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_KA50.lua');
 			elseif gCurrentAircraft == "Mi-8MT" then
-				la_petite_chatte = '/Scripts/KTZ_SIOC_Mi8.lua'
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_Mi8.lua');
 			elseif gCurrentAircraft == "UH-1H" then
-				la_petite_chatte = '/Scripts/KTZ_SIOC_UH1.lua'
-			end
-		end
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_UH1.lua');
+			elseif gCurrentAircraft == "P-51D" then
+			else
+				dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_FC3.lua');
+			end   
+		end  
 	elseif not exportFC3done then
 		exportFC3done = true
-		la_petite_chatte = '/Scripts/KTZ_SIOC_FC3.lua'
+		dofile(lfs.writedir()..'/Scripts/KTZ_SIOC_FC3.lua');
 	end
-	if la_petite_chatte then
-		if remplir_la_gamelle_de(la_petite_chatte) then
-			changer_la_litiere_de(la_petite_chatte)
-		end
-	end
-end
-
-function remplir_la_gamelle_de(chaton)
-	local f=io.open(lfs.writedir()..chaton,'r')
-	if f~=nil then io.close(f) return  true else return false end
-end
-
-function changer_la_litiere_de(vieux_matou)
-	dofile(lfs.writedir()..vieux_matou);
 end
