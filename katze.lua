8d7a2d328909ec535075c176095526e8236bb179
etcher3rd <etcher3rd@users.noreply.github.com>
2015-02-17 09:12:37 +0100

    Update katze.lua
    ajout de la fonction log

--- katze.lua
+++ katze.lua
@@ -1,6 +1,34 @@
 k = {} -- création de la "master table"
 k.export_fc3_done = false -- l'export de FC3 a déjà été fait, ne plus le refaire en boucle pour rien
 k.current_aircraft = nil -- appareil dans lequel l'utilisateur se trouve actuellement
+k.c -- socket
+k.log_file = nil
+
+k.k_log = function (message)
+
+	-- Création du fichier de log des communication serveur, s'il n'existe pas
+	-- Format , KTZ-SIOC3000_ComLog-yyyymmdd-hhmm.csv
+	--
+	if DEBUG_MODE and not k.log_file then
+       		k.log_file = io.open(lfs.writedir().."Logs\\KatzePit\\KTZ-SIOC5010_ComLog-"..os.date("%Y%m%d-%H%M")..".csv", "w");
+				
+		-- Ecriture de l'entète dans le fichier
+		if k.log_file then
+			
+			k.log_file:write("*********************************************;\n");
+			k.log_file:write("*     Fichier Log des Communications SIOC   *;\n");
+			k.log_file:write("*     Par KaTZe  -  http://www.3rd-wing.net *;\n");
+			k.log_file:write("*     Version FC3  du 02/02/2015            *;\n");
+			k.log_file:write("*********************************************;\n\n");
+		end
+	end
+	
+	-- Ecriture des données dans le fichier existant
+	if k.log_file then
+	--fichierComLog:write(string.format(" %s ; %s",os.date("%d/%m/%y %H:%M:%S"),message),"\n");
+		k.log_file:write(string.format(" %s ; %s",os.clock(),message),"\n");
+	end
+end
 
 function rendre_hommage_au_grand_Katze()
 	local todo_file = false -- fichier à lire
@@ -36,3 +64,5 @@ function file_exists(chaton)
 	local f=io.open(lfs.writedir()..chaton,'r')
 	if f~=nil then io.close(f) return  true else return false end
 end
+
+dofile ( lfs.writedir().."Scripts\\siocConfig.lua" )
