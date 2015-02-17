cba35210cde767a3c206c6e324fcb925219e70f1
etcher3rd <etcher3rd@users.noreply.github.com>
2015-02-17 09:52:35 +0100

    Update katze.lua

--- katze.lua
+++ katze.lua
@@ -17,24 +17,27 @@ k.log = function (message)
 	-- Création du fichier de log des communication serveur, s'il n'existe pas
 	-- Format , KTZ-SIOC3000_ComLog-yyyymmdd-hhmm.csv
 	--
-	if DEBUG_MODE and not k.log_file then
-       		k.log_file = io.open(lfs.writedir().."Logs\\KatzePit\\KTZ-SIOC5010_ComLog-"..os.date("%Y%m%d-%H%M")..".csv", "w");
+	if DEBUG_MODE then
+		
+		if not k.log_file then
+			-- création du fichier log si nécessaire
+       			k.log_file = io.open(lfs.writedir().."Logs\\KatzePit\\KTZ-SIOC5010_ComLog-"..os.date("%Y%m%d-%H%M")..".csv", "w");
 				
-		-- Ecriture de l'entète dans le fichier
-		if k.log_file then
+			-- Ecriture de l'entète dans le fichier
+			if k.log_file then
+				k.log_file:write("*********************************************;\n");
+				k.log_file:write("*     Fichier Log des Communications SIOC   *;\n");
+				k.log_file:write("*     Par KaTZe  -  http://www.3rd-wing.net *;\n");
+				k.log_file:write("*     Version FC3  du 02/02/2015            *;\n");
+				k.log_file:write("*********************************************;\n\n");
+			end
 			
-			k.log_file:write("*********************************************;\n");
-			k.log_file:write("*     Fichier Log des Communications SIOC   *;\n");
-			k.log_file:write("*     Par KaTZe  -  http://www.3rd-wing.net *;\n");
-			k.log_file:write("*     Version FC3  du 02/02/2015            *;\n");
-			k.log_file:write("*********************************************;\n\n");
 		end
-	end
 	
-	-- Ecriture des données dans le fichier existant
-	if k.log_file then
-	--fichierComLog:write(string.format(" %s ; %s",os.date("%d/%m/%y %H:%M:%S"),message),"\n");
-		k.log_file:write(string.format(" %s ; %s",os.clock(),message),"\n");
+		-- Ecriture des données dans le fichier existant
+		if k.log_file then
+			k.log_file:write(string.format(" %s ; %s",os.clock(),message),"\n");
+		end
 	end
 end
 
